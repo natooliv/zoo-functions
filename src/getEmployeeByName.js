139bd99ee37701dc932aaa22a00398d00ea45bc4
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-10-31 17:34:47 -0300

    3 - Implemente a função getEmployeeByName.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const { employees } = data;
 
+function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) return {};
+  const location = employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  return location;
+}
+// console.log(getEmployeeByName('Burl'));
 module.exports = getEmployeeByName;
