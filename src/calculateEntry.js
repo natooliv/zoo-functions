cc83fb51b50b68a90fed9347ada5729e8cd00e3a
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-10-31 20:24:38 -0300

    8. Implemente a função calculateEntry.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui.
+  return {
+    child: entrants.filter(({ age }) => age < 18).length,
+    adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+    senior: entrants.filter(({ age }) => age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const numeroDeVisitantes = countEntrants(entrants);
+  const numeroCrianca = numeroDeVisitantes.child * data.prices.child;
+  const numeroAdult = numeroDeVisitantes.adult * data.prices.adult;
+  const numeroIdoso = numeroDeVisitantes.senior * data.prices.senior;
+  return numeroCrianca + numeroAdult + numeroIdoso;
 }
 
 module.exports = { calculateEntry, countEntrants };
