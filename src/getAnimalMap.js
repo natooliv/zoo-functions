ad66641f7140180bbe9d8b2ef790a572a7f8b21a
Natalia Oliveira <natoliveira7300@hotmail.com>
2022-11-01 11:32:05 -0300

    Implemente a função getAnimalMap e acertos.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const animals = (local) => data.species.filter((obj) => obj.location === local).map((a) => a.name);
+
+const verifySexOrSort = (local, opt) => data.species.filter((obj) => obj.location === local)
+  .map((animal) => ({ [animal.name]: animal.residents
+    .filter((resid) => !opt.sex || opt.sex === resid.sex)
+    .map((obj) => obj.name)
+    .sort((a, b) => ((opt.sorted) ? a.localeCompare(b) : 0)) }));
+
+const chooseOptions = (opt, fn) => data.species.map((obj) => obj.location)
+  .reduce((a, b) => {
+    if (!a.includes(b)) a.push(b);
+    return a;
+  }, []).reduce((a, b) => ({ ...a, [b]: fn(b, opt) }), {});
+
+const getAnimalMap = (options) => ((!options || !options.includeNames)
+  ? chooseOptions(options, animals) : chooseOptions(options, verifySexOrSort));
+
+// console.log();
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const vezesDois = arr.reduce((acc, value) => {
+  // [...acc, num * 2]
+  acc.push(value * 2);
+  return acc;
+}, []);
+console.log(vezesDois);
 
 module.exports = getAnimalMap;
--- src/getOpeningHours.js
+++ src/getOpeningHours.js
@@ -1,26 +1,26 @@
 const { hours } = require('../data/zoo_data');
 
-const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const dayError = 'The day must be valid. Example: Monday';
+const diasdaSemana = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const erroDia = 'The day must be valid. Example: Monday';
 
-const isStringRepresentNumber = (string, what) => {
+const stringNumber = (string, what) => {
   if (!/^\d+$/.test(string)) {
     throw new Error(`The ${what} should represent a number`);
   }
 };
 
-const validateAbbreviation = (abbreviation) => {
+const validacao = (abbreviation) => {
   if (!['AM', 'PM'].includes(abbreviation)) {
     throw new Error('The abbreviation must be \'AM\' or \'PM\'');
   }
 };
 
-const validateHour = (hour) => {
+const validacaoHora = (hour) => {
   const [number, abbreviation] = hour.toUpperCase().split('-');
   const [dataHours, dataMinutes] = number.split(':');
-  isStringRepresentNumber(dataHours, 'hour');
-  isStringRepresentNumber(dataMinutes, 'minutes');
-  validateAbbreviation(abbreviation);
+  stringNumber(dataHours, 'hour');
+  stringNumber(dataMinutes, 'minutes');
+  validacao(abbreviation);
   switch (false) {
   case Number(dataHours) >= 0 && Number(dataHours) <= 12:
     throw new Error('The hour must be between 0 and 12');
@@ -31,9 +31,9 @@ const validateHour = (hour) => {
   }
 };
 
-const validateDay = (day) => {
-  if (!weekDays.includes(day)) {
-    throw new Error(dayError);
+const validacaoData = (day) => {
+  if (!diasdaSemana.includes(day)) {
+    throw new Error(erroDia);
   }
 };
 
@@ -53,14 +53,14 @@ const openOrClosed = (period, hour, open, close) => {
 const getOpeningHours = (day, dataHour) => {
   if (empty(day, dataHour)) return hours;
   const adjustedDay = `${day[0].toUpperCase()}${day.slice(1).toLowerCase()}`;
-  validateDay(adjustedDay);
-  validateHour(dataHour);
+  validacaoData(adjustedDay);
+  validacaoHora(dataHour);
   const { open, close } = hours[adjustedDay];
   if (empty(close, open)) return 'The zoo is closed';
-  const period = dataHour.split('-')[1].toUpperCase();
+  const periodo = dataHour.split('-')[1].toUpperCase();
   const hour = Number(dataHour.split(':')[0]);
   let message = 'The zoo is ';
-  message += openOrClosed(period, hour, open, close) ? 'open' : 'closed';
+  message += openOrClosed(periodo, hour, open, close) ? 'open' : 'closed';
   return message;
 };
 
